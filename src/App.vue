8b262277addc65845e3a2aaeca29d510f04d3fd3
unknown <n.niedziela201@gmail.com>
2020-09-24 14:38:29 +0200

    live server add

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="container">
-    
+        <enterQuote></enterQuote>
     </div>
 </template>
 
 <script>
+import enterQuote from './EnterQuote.vue';
+
     export default {
+        components: {
+            enterQuote: 'enterQuote',
+        }
         
     }
 </script>
---
+++ src/enterQuote.vue
@@ -0,0 +1,20 @@
+<template>
+   <div>
+        <h1>Lubie placki</h1>
+   </div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
